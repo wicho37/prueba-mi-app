358feabf9064d20a4b8ab4e4a36326bbd08baa53
wicho37 <willihernandezramirez@gmail.com>
2026-04-23 08:52:20 -0500

    cambios en el buscador con css, se añade imagen

--- src/components/buscador/Buscador.tsx
+++ src/components/buscador/Buscador.tsx
@@ -1,41 +1,19 @@
-import React from "react";
+import style from "./Buscador.module.css"
+import lente from "../../assets/imagenes/lupa.png" 
 
 
 
 const Buscador = () => {
+  
   return (
-    <div>
-      <div>
-        
-            <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        cx="11"
-        cy="11"
-        r="7"
-        stroke="black"
-        strokeWidth="2"
-      />
-      <line
-        x1="16"
-        y1="16"
-        x2="22"
-        y2="22"
-        stroke="black"
-        strokeWidth="2"
-      />
-    </svg>
-
-      </div>
+    <div className={style.buscador}>
       <input
         type="text"
-        placeholder="Buscar tu producto"
+        placeholder="Buscar producto"
       />
+      <button className={style.btnBuscar}>
+        <img src={lente} alt="buscar" />
+      </button>
     </div>
   );
 };
